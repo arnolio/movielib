e7c5490a81c189f637117a4d368cbb617fc6fc61
Jerry Arne <jerry.arne@gmail.com>
2019-07-18 01:12:13 -0400

    Clean up API controller logic Finish implementing sort

--- controllers/movie.js
+++ controllers/movie.js
@@ -2,53 +2,83 @@ Store = require('yayson')().Store;
 store = new Store();
 
 module.exports = (app, db, presenter) => {
-    app.get( "/movie", (req, res, next) =>
-            db.movie.findAll().then((result) => {
-                res.json(presenter.render(result));
-            }).catch((err) => next(err))
+    app.get( "/movies", (req, res, next) => {
+        let condition = !req.query.sort ? {} : { order: getSortArray(req.query.sort) } ;
+        db.movie.findAll(condition).then((result) => {
+            res.json(presenter.render(result));
+        }).catch((err) => next(err))
+
+    });
+
+    app.get( "/movies/:id", getMovie, (req, res) =>
+        res.json(presenter.render(req.movie))
     );
 
-    app.get( "/movie/:id", (req, res, next) =>
-        db.movie.findByPk(req.params.id).then(
-            (result) => {
-                if (!result) {
-                    res.status(404)
-                        .send('Not found')
-                }
-                res.json(presenter.render(result))
-            }
-        ).catch((err) => next(err))
-    );
-
-    app.post("/movie", (req, res, next) => {
-            let movie = store.sync(req.body);
-            db.movie.create(movie).then(
+    app.post("/movies", (req, res, next) => {
+            let payload = getPayload(req.body);
+            db.movie.create(payload).then(
                 (result) => res.json(presenter.render(result))
             ).catch((err) => next(err))
         }
     );
 
-    app.put( "/movie/:id", (req, res, next) => {
-            let movie = store.sync(req.body);
-            db.movie.update(movie, {
-                where: {
-                    id: req.params.id
-                }
-            }).then(
-                (result) => res.json(presenter.render(result))
-            ).catch((err) => next(err))
+    app.put( "/movies/:id", getMovie, (req, res, next) => {
+        let payload = getPayload(req.body);
+        req.movie.update(payload)
+            .then((self) => res.json(presenter.render(self)))
+            .catch((err) => next(err));
         }
     );
 
-    app.delete( "/movie/:id", (req, res, next) =>
+    app.delete( "/movies/:id", (req, res, next) =>
         db.movie.destroy({
             where: {
                 id: req.params.id
             }
         }).then(
-            (result) => res.json(presenter.render(result))
+            () => res.status(204)
         ).catch((err) => {
             next(err)
         })
     );
+
+    app.use(function(error, req, res, next) {
+        if (res.headersSent) {
+            return next(error)
+        }
+        res.status(500).json({ error: error.toString() });
+    });
+
+    function getSortArray(sortString) {
+        let sortArray = [], descending;
+        sortString.split(",").forEach((sortColumn) => {
+            descending = sortColumn.charAt(0) === '-';
+            sortArray.push([
+                (descending ? sortColumn.slice(1) : sortColumn),
+                (descending ? 'DESC' : 'ASC')
+            ])
+        });
+        return sortArray;
+    }
+
+    function getMovie(req, res, next) {
+        db.movie.findByPk(req.params.id).then((result) => {
+            if (!result) {
+                res.status(404)
+                    .send('Movie Not Found')
+            }
+            req.movie = result;
+            next();
+        })
+    }
+
+    function getPayload(body) {
+        let payload = store.sync(body);
+        delete payload.createdAt;
+        delete payload.updatedAt;
+        delete payload.type;
+        delete payload.id;
+
+        return payload;
+    }
 };
